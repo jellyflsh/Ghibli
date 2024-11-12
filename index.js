d0dddc1963ae2f5c1964a95de67881025b121d18
fruitsrouges <justine.qu@outlook.fr>
2024-11-12 12:59:56 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,1 @@
-const barCanvas = document.getElementById("barCanvas");
 
-const barChart = new Chart(barCanvas, {
-    type: "bar",
-    data: {
-        labels: ["Beijing", "Tokyo", "Seoul", "Hong Kong"],
-        datasets: [{
-            data: []
-        }]
-    }
-})
